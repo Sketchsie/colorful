eca6674ee0ec111be6d86340cf026df7553014f6
LaksCastro <lakhsmycastro336@gmail.com>
2020-03-26 14:31:49 -0300

    change card pallete grid strutcture

--- src/components/PaletteCardWrapper/index.js
+++ src/components/PaletteCardWrapper/index.js
@@ -9,21 +9,26 @@ function PaletteCardWrapper(colorsArray) {
     const dataAppear = generateRandomNumber(0, 1) === 1 ? "right" : "left";
 
     const paletteString = `
-       <section class="p-item-container" data-appear="${dataAppear}">
-       
-       </section>
+       <section class="p-item-container" data-appear="${dataAppear}"></section>
+    `
+    const paletteButtonString = `
+       <button class="p-item-button">
+        <i class="fas fa-heart"></i>
+       </button>
     `
     const paletteContainerColorsString = `
-        <div class="p-item-colors-wrapper"></div>
+        <div class="p-item-colors-wrapper" data-appear="${dataAppear}"></div>
     `
     const paletteNode = paletteString.stringToHTML();
+    const paletteButtonNode = paletteButtonString.stringToHTML();
     const paletteContainerColorsNode = paletteContainerColorsString.stringToHTML();
-    
+
     colorsArray.forEach(color => {
         paletteContainerColorsNode.appendChild(PaletteCard(color));
     });
 
     paletteNode.appendChild(paletteContainerColorsNode);
+    paletteNode.appendChild(paletteButtonNode);
 
     return paletteNode;
 }
