34d6dd7a39feab73a487694135a3c8c5c6d608bc
LaksCastro <lakhsmycastro336@gmail.com>
2020-03-26 20:09:00 -0300

    color fix on interface preview

--- src/components/InterfacePreview/index.js
+++ src/components/InterfacePreview/index.js
@@ -1,5 +1,7 @@
 import "./styles.scss";
 
+import { lightOrDark } from "../../scripts/utils";
+
 const interfacePreview = function () {
     const interfaceButtonBackString = `
         <button id="interface-preview-button-back">
@@ -38,14 +40,25 @@ const interfacePreview = function () {
         const interfaceHeader = document.getElementById("interface-preview-container-title");
         interfaceHeader.style.backgroundColor = palette[1];
 
+
         const interfaceBody = document.getElementById("interface-preview-span-container");
         interfaceBody.style.backgroundColor = palette[3];
 
+
         const interfaceTabs = document.getElementById("interface-preview-tabs-container");
+
+        const useDarkColorInTabs = lightOrDark(palette[4]) === "light";
+
         interfaceTabs.style.backgroundColor = palette[4];
+        interfaceTabs.style.color = useDarkColorInTabs ? "#000" : "#fff";
+
 
         const interfaceDemoButton = document.getElementById("interface-preview-button-add");
+
+        const useDarkColorInButton = lightOrDark(palette[3]) === "light";
+
         interfaceDemoButton.style.backgroundColor = palette[3];
+        interfaceDemoButton.style.color = useDarkColorInButton ? "#000" : "#fff";
     }
     window.__closeInterfacePreview = function () {
         interfaceNode.classList.remove("visible");
