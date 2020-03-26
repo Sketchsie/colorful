24ae539054368452078c0859d074a2332685948f
LaksCastro <lakhsmycastro336@gmail.com>
2020-03-26 14:00:31 -0300

    fix ui styles

--- src/components/PaletteCardWrapper/index.js
+++ src/components/PaletteCardWrapper/index.js
@@ -9,12 +9,21 @@ function PaletteCardWrapper(colorsArray) {
     const dataAppear = generateRandomNumber(0, 1) === 1 ? "right" : "left";
 
     const paletteString = `
-       <section class="p-item-container" data-appear="${dataAppear}"></section>
+       <section class="p-item-container" data-appear="${dataAppear}">
+       
+       </section>
+    `
+    const paletteContainerColorsString = `
+        <div class="p-item-colors-wrapper"></div>
     `
     const paletteNode = paletteString.stringToHTML();
+    const paletteContainerColorsNode = paletteContainerColorsString.stringToHTML();
+    
     colorsArray.forEach(color => {
-        paletteNode.appendChild(PaletteCard(color));
+        paletteContainerColorsNode.appendChild(PaletteCard(color));
     });
+
+    paletteNode.appendChild(paletteContainerColorsNode);
 
     return paletteNode;
 }
