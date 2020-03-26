fa4f6ab11f0af42c73350c9793cab3e49fd4ab68
LaksCastro <lakhsmycastro336@gmail.com>
2020-03-26 15:18:30 -0300

    add nav tabs color

--- src/components/NavTabs/tabs.js
+++ src/components/NavTabs/tabs.js
@@ -1,25 +1,22 @@
 const tabs = [{
     name: "Dark",
-    icons: ["#1b040b", "#0b6d05", "#16d006", "#a44d53"]
-},{
+    icons: ["#301443", "#491562", "#501880", "#a57edb"]
+}, {
     name: "Light",
-    icons: ["#1b040b", "#0b6d05", "#16d006", "#a44d53"]
-},{
-    name: "Sun",
-    icons: ["#1b040b", "#0b6d05", "#16d006", "#a44d53"]
-},{
+    icons: ["#01a9be", "#02cbdf", "#04e6fc", "#e5fffd"]
+}, {
+    name: "Sunset",
+    icons: ["#2a1a5e", "#f45905", "#fb9224", "#fbe555"]
+}, {
     name: "Night",
-    icons: ["#1b040b", "#0b6d05", "#16d006", "#a44d53"]
-},{
+    icons: ["#392ca3", "#5a32fa", "#774bfd", "#8462ff"]
+}, {
     name: "Void",
-    icons: ["#1b040b", "#0b6d05", "#16d006", "#a44d53"]
-},{
+    icons: ["#252a42", "#2b3751", "#353b53", "#afd8bb"]
+}, {
     name: "My Palette's",
-    icons: ["#1b040b", "#0b6d05", "#16d006", "#a44d53"]
-},{
-    name: "My Color's",
-    icons: ["#1b040b", "#0b6d05", "#16d006", "#a44d53"]
-},{
+    icons: ["#9a274b", "#db3765", "#f44069", "#ff8ca7"]
+}, {
     name: "Tips",
     icons: ["#1b040b", "#0b6d05", "#16d006", "#a44d53"]
 }]
