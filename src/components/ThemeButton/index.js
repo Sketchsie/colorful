8dd17704681476c29223ef6729e252c059dec885
LaksCastro <lakhsmycastro336@gmail.com>
2020-03-25 21:47:06 -0300

    add button theme logic, toggle mode

--- src/components/ThemeButton/index.js
+++ src/components/ThemeButton/index.js
@@ -3,10 +3,11 @@ import "./styles.scss";
 function ThemeButton() {
     const buttonString = `
         <button id="theme-button">
-            <i class="far fa-lightbulb"></i>
+            <i class="fas fa-paint-roller"></i>
         </button>
     `
     const buttonNode = buttonString.stringToHTML();
+    buttonNode.onclick = () => window.__setPreferredTheme(window.__theme === "dark" ? "light" : "dark");
     return buttonNode;
 }
 
