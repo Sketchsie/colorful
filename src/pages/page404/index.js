69de98d43485008359202b843d37e68c8018efc7
LaksCastro <lakhsmycastro336@gmail.com>
2020-03-28 04:32:55 -0300

    added 404 custom page

--- src/pages/page404/index.js
+++ src/pages/page404/index.js
@@ -1,9 +1,28 @@
-import IndexTemplate from "../index-template"
+import "./styles.scss";
+
+import "../../assets/images/404.svg";
+
+import { removeAllChildsOf } from "../../scripts/utils";
+
+import { redirect } from "../../scripts/redirectTo"
 
 const IndexPage = {
     render: function () {
-        document.getElementById("root").innerHTML = "Page 404";
 
+        const page404 = `
+            <div id="page-404">
+                <h1>40</h1>
+                <p>Nothing here, sorry</p>
+                <button id="go-back-button">Go Back</button>
+                <img src="${process.env.PATH_BASE === "/" ? "/assets/404.svg" : `${process.env.PATH_BASE}/assets/404.svg`}" />
+            </div>
+        `
+        const rootElement = document.getElementById("root")
+
+        removeAllChildsOf(rootElement);
+        rootElement.appendChild(page404.stringToHTML());
+
+        document.getElementById("go-back-button").onclick = () => redirect("/");
     }
 }
 
