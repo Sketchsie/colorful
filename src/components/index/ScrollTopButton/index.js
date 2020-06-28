7d643d857665db521c63c5a6c43b5fab258c04c3
Laks Castro <lakhsmycastro336@gmail.com>
2020-06-28 13:40:57 -0300

    Update index.js

--- src/components/index/ScrollTopButton/index.js
+++ src/components/index/ScrollTopButton/index.js
@@ -6,6 +6,7 @@ function ScrollTopButton() {
             <i class="fas fa-arrow-up"></i>
         </button>
     `
+    
     const buttonNode = buttonString.stringToHTML();
     buttonNode.onclick = () => window.scroll({ top: 0, left: 0, behavior: "smooth" });
   
