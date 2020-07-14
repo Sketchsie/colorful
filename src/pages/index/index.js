7ffa8733c8f147f360a7899eb4e51708ea6710b9
Laks Castro <lakhsmycastro336@gmail.com>
2020-07-14 11:24:46 -0300

    Update index.js

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -5,21 +5,25 @@ import { generateRandomColor, getValidRgbValue } from "../../scripts/utils";
 
 function generatePalette() {
     const base = generateRandomColor();
+  
     const baseDark = generateRandomColor({
         r: [0, base.color.r / 2],
         g: [0, base.color.g / 2],
         b: [0, base.color.b / 2],
     });
+  
     const baseSemiDark = generateRandomColor({
         r: [base.color.r / 2, base.color.r],
         g: [base.color.g / 2, base.color.g],
         b: [base.color.b / 2, base.color.b],
     });
+  
     const baseSemiLight = generateRandomColor({
         r: [base.color.r, getValidRgbValue(base.color.r + 30)],
         g: [base.color.g, getValidRgbValue(base.color.g + 30)],
         b: [base.color.b, getValidRgbValue(base.color.b + 30)],
     });
+  
     const baseLight = generateRandomColor({
         r: [getValidRgbValue(base.color.r + 30), 255],
         g: [getValidRgbValue(base.color.g + 30), 255],
